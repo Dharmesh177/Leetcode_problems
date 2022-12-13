180abb8bb6c0a7036d39d2eebf43ae5265cf0232
Dharmesh Vala <77879603+Dharmesh177@users.noreply.github.com>
2022-12-13 15:07:33 +0530

    Time: 39 ms (30.78%), Space: 10.2 MB (66.30%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -13,13 +13,49 @@ public:
         return dp[i][j] = min(u,min(ld,rd));
     }
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        int n=matrix.size();
-        int m=matrix[0].size();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        int maxi=1e8;
-        for(int j=0; j<m; j++){
-            maxi=min(maxi,f(n-1,j,matrix,dp));
+        // int n=matrix.size();
+        // int m=matrix[0].size();
+        // vector<vector<int>> dp(n,vector<int>(m,-1));
+        // int maxi=1e8;
+        // for(int j=0; j<m; j++){
+        //     maxi=min(maxi,f(n-1,j,matrix,dp));
+        // }
+        // return maxi;
+        
+         int n = matrix.size();
+    int m = matrix[0].size();
+    
+    vector<vector<int>> dp(n,vector<int>(m,0));
+    
+    // Initializing first row - base condition
+    for(int j=0; j<m; j++){
+        dp[0][j] = matrix[0][j];
+    }
+    
+    for(int i=1; i<n; i++){
+        for(int j=0;j<m;j++){
+            
+            int up = matrix[i][j] + dp[i-1][j];
+            
+            int leftDiagonal= matrix[i][j];
+            if(j-1>=0) leftDiagonal += dp[i-1][j-1];
+            else leftDiagonal += 1e9;
+            
+            int rightDiagonal = matrix[i][j];
+            if(j+1<m) rightDiagonal += dp[i-1][j+1];
+            else rightDiagonal += 1e9;
+            
+            dp[i][j] = min(up, min(leftDiagonal,rightDiagonal));
+            
         }
-        return maxi;
+    }
+    
+    int maxi = INT_MAX;
+    
+    for(int j=0; j<m;j++){
+        maxi = min(maxi,dp[n-1][j]);
+    }
+    
+    return maxi;
     }
 };
