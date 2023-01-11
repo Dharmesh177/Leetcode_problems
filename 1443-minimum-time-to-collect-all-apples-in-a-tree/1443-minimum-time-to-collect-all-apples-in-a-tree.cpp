822076123258927ff1937d48292af3e6e5ef151e
Dharmesh Vala <77879603+Dharmesh177@users.noreply.github.com>
2023-01-11 11:04:04 +0530

    Time: 207 ms (73.15%), Space: 65 MB (41.28%) - LeetHub

--- 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
 
-      int dfs(            vector<vector<int>> &adj
-,vector<bool>& hasApple,int node,int d,int prev)
-    {
-        int result=0,temp;
-        for(int &i:adj[node])
-	    if(i!=prev)
-	    {
-	        temp=dfs(adj,hasApple,i,d+1,node);
-	        if(temp) result+=temp-d;
-	    }
-        return result||hasApple[node]?result+d:0; 
-        
+       int dfs(int node,vector<vector<int>> &adj,vector<bool> &visited,vector<bool> &hasApple){
+        visited[node]=1;
+        int sum=0;
+        for(auto i:adj[node]){
+            if(!visited[i]){
+                sum+=dfs(i,adj,visited,hasApple);
+            }
+        }
+        if(sum==0){
+            if(hasApple[node]){return 2;}
+            else{return 0;}
+        }
+        else{
+            return sum+2;
+        }
     }
-    
     int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple) {
                     vector<vector<int>> adj(n);
 
@@ -22,8 +24,10 @@ public:
             adj[x[0]].push_back(x[1]);
             adj[x[1]].push_back(x[0]);
         }
-         return dfs(adj,hasApple,0,0,-1)*2;
-
+        vector<bool> visited(n,0);
+        int ans=dfs(0,adj,visited,hasApple);
+        if(ans==0){return 0;}
+        return ans-2;
         
     }
 };
