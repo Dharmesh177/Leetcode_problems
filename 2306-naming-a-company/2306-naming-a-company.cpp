f79c3fe825f0132f491f60def6c2ed87085b93b1
Dharmesh Vala <77879603+Dharmesh177@users.noreply.github.com>
2023-02-09 11:11:53 +0530

    Time: 2643 ms (5.69%), Space: 426.9 MB (5.69%) - LeetHub

--- 2306-naming-a-company/2306-naming-a-company.cpp
+++ 2306-naming-a-company/2306-naming-a-company.cpp
@@ -1,30 +1,19 @@
 class Solution {
 public:
     long long distinctNames(vector<string>& ideas) {
-         long long ans=0;
-       int len=ideas.size();
-       int  cnt[26][26]={0};
-       unordered_set<string> Set;
-       for(int j=0;j<len;j++)
-       {
-
-           Set.insert(ideas[j]);
-       }
-       for(int j=0;j<len;j++){
-           string s= ideas[j];
-           int st=s[0] -'a';
-           
-           for(int i=0;i<26;i++)
-           {
-              s[0]=i+'a';
-             if (Set.find(s)==Set.end())
-             {
-                   cnt[st][i]++;  
-                   ans+=cnt[i][st];
-               }
-           }
-       }
-     
-       return ans*2;
+         long long disName = 0;
+        vector<unordered_set<string>> arr(26);
+        for (string s : ideas) 
+            arr[s[0] - 'a'].insert(s.substr(1));
+        
+        for (int i = 0; i < 25; i++) {
+            for (int j = i + 1; j < 26; j++) {
+                unordered_set<string> set;
+                set.insert(arr[i].begin(), arr[i].end());
+                set.insert(arr[j].begin(), arr[j].end());
+                disName += (arr[i].size() - set.size()) * (arr[j].size() - set.size());
+            }
+        }
+        return disName * 2;
     }
 };
