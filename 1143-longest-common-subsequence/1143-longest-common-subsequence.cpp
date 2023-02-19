c549bf10c44d7a02937a88a170fc40f5242767a3
Dharmesh Vala <77879603+Dharmesh177@users.noreply.github.com>
2023-02-19 19:48:30 +0530

    Time: 41 ms (59.22%), Space: 19 MB (12.41%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,43 +1,19 @@
 class Solution {
 public:
-    int lcs(int i,int j,string text1, string text2,vector<vector<int>> &dp){
-         int n =text1.size();
-        int m=text2.size();
-         if(i<0 || j<0){
-            return 0;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        if(text1[i]==text2[j]){
-            return dp[i][j] =  1 + lcs(i-1,j-1,text1,text2,dp);
-        }
-        return dp[i][j] =  max(lcs(i-1,j,text1,text2,dp),lcs(i,j-1,text1,text2,dp));
+    int f(int i1,int i2,string &text1,string &text2,vector<vector<int>> &dp){
+        if(i1<0 || i2<0) return 0;
         
-       
+        if(dp[i1][i2] !=-1) return dp[i1][i2];
+        
+        if(text1[i1]==text2[i2]){
+            return dp[i1][i2] = 1+f(i1-1,i2-1,text1,text2,dp);
+        }
+        return dp[i1][i2] =  0 + max(f(i1-1,i2,text1,text2,dp),f(i1,i2-1,text1,text2,dp));
     }
     int longestCommonSubsequence(string text1, string text2) {
-        int n =text1.size();
-        int m=text2.size();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        int n1=text1.size(),n2=text2.size();
         
-        for(int i=0; i<=n; i++){
-            dp[i][0]=0;
-        }
-        for(int j=0; j<=m; j++){
-            dp[0][j]=0;
-        }
-        for(int ind1=1;ind1<=n;ind1++){
-        for(int ind2=1;ind2<=m;ind2++){
-            if(text1[ind1-1]==text2[ind2-1])
-                dp[ind1][ind2] = 1 + dp[ind1-1][ind2-1];
-            else
-                dp[ind1][ind2] = 0 + max(dp[ind1-1][ind2],dp[ind1][ind2-1]);
-        }
-    }
-        // int ans=lcs(n-1,m-1,text1,text2,dp);
-        return dp[n][m];
-        
-        
+       vector<vector<int>> dp(n1,vector<int>(n2,-1));
+       return f(n1-1,n2-1,text1,text2,dp);
     }
 };
