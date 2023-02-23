8ec9e042086cbd81954bb8dd4696fd938467785b
Dharmesh Vala <77879603+Dharmesh177@users.noreply.github.com>
2023-02-23 10:11:42 +0530

    Time: 221 ms (84.29%), Space: 78 MB (90.99%) - LeetHub

--- 0502-ipo/0502-ipo.cpp
+++ 0502-ipo/0502-ipo.cpp
@@ -1,34 +1,7 @@
 class Solution {
 public:
-    // int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
-        // priority_queue<int> q;
-        // vector<pair<int,int>> v;
-        // for(int i=0; i<profits.size(); i++){
-        //     v.push_back({profits[i],capital[i]});
-        
-        // }
-        // sort(v.begin(),v.end());
-        
-        // int ans = 0, i=0;
-        // while(i<v.size() and k){
-        //     if(w>=v[i].second) q.push(v[i++].first);
-        //     else{
-		// 		// This condition will check if we had enough capital to choose any project or not. If not, we directly return the current capital.
-        //         if(q.empty()) return w;
-                
-        //         w += q.top();
-        //         q.pop();
-        //         k--;
-        //     }
-        // }
-		// // Here, we check if we could still choose some projects
-        // while(k-- and !q.empty()){
-        //     w += q.top();
-        //     q.pop();
-        // }
-
-        int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
-    int n=profits.size();
+    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
+        int n=profits.size();
     vector<pair<int,int>> projects(n);
     for(int i=0;i<n;i++) projects[i]={profits[i],capital[i]}; 
     int i=0;
@@ -43,6 +16,5 @@ public:
         if(!maxProfit.empty()) w+=maxProfit.top(),maxProfit.pop();
     }
     return w;
-}
-    
+    }
 };
