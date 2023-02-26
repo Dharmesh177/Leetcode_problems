45942f499967efdb012651fadba5ae6c31425531
Dharmesh Vala <77879603+Dharmesh177@users.noreply.github.com>
2023-02-26 20:37:13 +0530

    Time: 729 ms (44.18%), Space: 207.1 MB (35.82%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -19,28 +19,29 @@ public:
                vector<vector<int>>(2,
                       vector<int>(3,0)));
         
-       
-        // int profit = 0;
-         for(int ind = n-1; ind>=0; ind--){
-        for(int buy = 0; buy<=1; buy++){
-            for(int cap=1; cap<=2; cap++){
-                
-                if(buy==0){// We can buy the stock
-                    dp[ind][buy][cap] = max(0+dp[ind+1][0][cap], 
-                                -prices[ind] + dp[ind+1][1][cap]);
-                 }
-    
-                if(buy==1){// We can sell the stock
-                    dp[ind][buy][cap] = max(0+dp[ind+1][1][cap],
-                                prices[ind] + dp[ind+1][0][cap-1]);
+//         for(int ind=0; ind<n; ind++){
+//             for(int buy=0; buy<2; buy++){
+//                 dp[ind][buy][0] = 0;
+//             }
+//         }
+        
+//         for(int buy=0; buy<2; buy++){
+//             for(int cap=0; buy<3; cap++){
+//                 dp[n][buy][cap] = 0;
+//             }
+//         }
+//         int profit = 0;
+        for(int i=n-1; i>=0; i--){
+            for(int buy=0; buy<=1; buy++){
+                for(int cap=1; cap<=2; cap++){
+                     if(buy==1){
+            dp[i][buy][cap]=max(-prices[i]+dp[i+1][0][cap],0+dp[i+1][1][cap]);
+        }else{
+            dp[i][buy][cap]=max(prices[i]+dp[i+1][1][cap-1],0+dp[i+1][0][cap]);
+        }
                 }
             }
         }
-    }
-    
-    
-    // return dp[0][1][2];
-   
-        return dp[0][0][2];
+        return dp[0][1][2];
     }
 };
