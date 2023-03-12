cd0e054cffa61c9798251d65b6960a7258694d73
Dharmesh Vala <77879603+Dharmesh177@users.noreply.github.com>
2023-03-12 13:50:55 +0530

    Time: 27 ms (60.37%), Space: 13.3 MB (74.06%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -0,0 +1,44 @@
+class compare{
+     public:
+    bool operator()(ListNode* a,ListNode* b){
+        return a->val>b->val;
+    }
+    
+};
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& listArray) {
+        priority_queue<ListNode*,vector<ListNode*>,compare> minheap;
+    int k=listArray.size();
+    if(k==0){
+        return NULL;
+    }
+    
+    for(int i=0; i<k; i++){
+        if(listArray[i] != NULL){
+            minheap.push(listArray[i]);
+        }
+    }
+   ListNode* head=NULL;
+    ListNode* tail=NULL;
+    while(minheap.size()>0){
+        ListNode* tmp=minheap.top();
+        minheap.pop();
+        
+         if(tmp->next!=NULL){
+                minheap.push(tmp->next);
+            }
+        
+        if(head==NULL){
+            head=tmp;
+            tail=tmp;
+           
+        }else{
+            tail->next=tmp;
+            tail=tmp;
+           
+        }
+    }
+    return head;
+    }
+};
