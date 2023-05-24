3a627059049d58362c5882102fecad78194babd9
Dharmesh Vala <77879603+Dharmesh177@users.noreply.github.com>
2023-05-24 10:20:41 +0530

    Time: 29 ms (28.64%), Space: 13.9 MB (53.77%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
-    bool canFinish(int V, vector<vector<int>>& prerequisites) {
-       vector<int> adj[V];
-		for (auto it : prerequisites) {
-			adj[it[0]].push_back(it[1]);
-		}
+ 
+    bool canFinish(int V, vector<vector<int>>& pre) {
+        vector<int> adj[V];
+        for(auto it:pre){
+            adj[it[0]].push_back(it[1]);
+        }
+        vector<int> indegree(V,0);
 
-
-
-		vector<int> indegree(V,0);
-		for (int i = 0; i < V; i++) {
+        for (int i = 0; i < V; i++) {
 			for (auto it : adj[i]) {
 				indegree[it]++;
 			}
@@ -35,7 +34,10 @@ public:
 			}
 		}
 
-		if (topo.size() == V) return true;
-		return false;
+        if(topo.size() == V){
+            return true;
+        }
+        return false;
+		
     }
 };
